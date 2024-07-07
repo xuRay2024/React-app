370eb773ffc02035819175742207123a10480c32
Ray <ericxu2017@gmail.com>
2024-07-07 20:35:31 +0800

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import { useState } from "react";
+import produce from "immer";
 import Button from "./componments/Button/Button";
 import Alert from "./componments/Alert";
 import ListGroup from "./componments/ListGroup";
 import Like from "./componments/Like";
+import Message from "./Message";
+import NavBar from "./componments/NavBar";
+import Cart from "./componments/Cart";
+import ExpandTexts from "./componments/ExpandTexts";
 
 function App() {
+  const handleClick = () => {};
+
   return (
     <div>
-      <Button onClick={() => console.log("Clicked")}>My Button</Button>
+      <ExpandTexts>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque,
+        voluptatibus amet. Hic excepturi quidem ut voluptatem asperiores
+        delectus soluta quam, autem, modi rem dolore! Nostrum id laborum labore
+        doloremque itaque.
+      </ExpandTexts>
     </div>
   );
 }
--- src/Message.tsx
+++ src/Message.tsx
@@ -1,9 +1,9 @@
-//PascalCasing
-function Message() {
-  //JSX: Javascript XML
-  const name = "";
-  if (name) return <h1>Hi {name}</h1>;
-  return <h1>Hello World</h1>;
-}
+let count = 0;
+
+const Message = () => {
+  console.log("Message called", count);
+  count++;
+  return <div>Message {count}</div>;
+};
 
 export default Message;
