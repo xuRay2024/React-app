7b0113ebb987ec4ab29c97961831a0e8dbf27670
Ray <ericxu2017@gmail.com>
2024-07-11 19:42:14 +0800

    test

--- src/App.tsx
+++ src/App.tsx
@@ -8,19 +8,29 @@ import Message from "./Message";
 import NavBar from "./componments/NavBar";
 import Cart from "./componments/Cart";
 import ExpandTexts from "./componments/ExpandTexts";
+import Form from "./componments/Form";
+import ExpenseList from "./expense-tracker/ExpenseList";
 
 function App() {
-  const handleClick = () => {};
+  const [expenses01, setExpenses] = useState([
+    { id: 1, description: "aa", amount: 6, category: "Unknow" },
+    { id: 2, description: "bb", amount: 7, category: "Unknow" },
+    { id: 3, description: "cc", amount: 8, category: "Unknow" },
+    { id: 4, description: "dd", amount: 9, category: "Unknow" },
+  ]);
 
   return (
-    <div>
-      <ExpandTexts>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque,
-        voluptatibus amet. Hic excepturi quidem ut voluptatem asperiores
-        delectus soluta quam, autem, modi rem dolore! Nostrum id laborum labore
-        doloremque itaque.
-      </ExpandTexts>
-    </div>
+    <>
+      <div>
+        <ExpenseList
+          expenses={expenses01}
+          onDelete={(id) => setExpenses(expenses01.filter((e) => e.id !== id))}
+        />
+      </div>
+      <div>
+        <Form />
+      </div>
+    </>
   );
 }
 
