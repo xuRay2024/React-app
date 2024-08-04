1584b9f1ba1a5c531d69de6d907d65331dc69136
Ray <ericxu2017@gmail.com>
2024-08-04 16:52:54 +0800

    test

--- src/App.tsx
+++ src/App.tsx
@@ -18,96 +18,12 @@ import { CanceledError } from "./services/api-client";
 import userService, { Persons } from "./services/user-service";
 
 function App() {
-  const [person, setPersons] = useState<Persons[]>([]);
-  const [error, setError] = useState("");
-  const [isLoading, setLoading] = useState(false);
+  // const content = "内容";
+  const Title = "标题";
 
-  useEffect(() => {
-    setLoading(true);
-    const { request, cancel } = userService.getAllUsers();
-    request
-      .then((res) => {
-        setPersons(res.data);
-        setLoading(false);
-      })
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setError(err.message);
-        setLoading(false);
-      });
+  const flag = true;
 
-    // .finally(() => {
-    //   setLoading(false);
-    // });
-
-    return () => cancel();
-  }, []);
-
-  const deleteUser = (user: Persons) => {
-    const originalUsers = [...person];
-    setPersons(person.filter((u) => u.id !== user.id));
-
-    userService.deleteUser(user.id).catch((err) => {
-      setError(err.message);
-      setPersons(originalUsers);
-    });
-  };
-
-  const addUser = () => {
-    const originalUsers = [...person];
-    const newUser = { id: 0, name: "Ray" };
-    setPersons([newUser, ...person]);
-
-    userService
-      .addUser(newUser)
-      .then(({ data: savedUser }) => setPersons([savedUser, ...person]))
-      .catch((err) => {
-        setError(err.message);
-        setPersons(originalUsers);
-      });
-  };
-
-  const updateUsers = (user: Persons) => {
-    const originalUsers = [...person];
-    const updateUser = { ...user, name: user.name + "!" };
-    setPersons(person.map((u) => (u.id === user.id ? updateUser : u)));
-
-    userService.updateUser(updateUser).catch((err) => {
-      setError(err.message);
-      setPersons(originalUsers);
-    });
-  };
-
-  return (
-    <>
-      {error && <p className='text-danger'>{error}</p>}
-      {isLoading && <div className='spinner-border'></div>}
-      <button className='btn btn-primary mb-3' onClick={addUser}>
-        Add
-      </button>
-      <ul className='list-group'>
-        {person.map((user) => (
-          <li
-            key={user.id}
-            className='list-group-item d-flex justify-content-between'>
-            {user.name}
-            <div>
-              <button
-                className='btn btn-outline-secondary mx-1'
-                onClick={() => updateUsers(user)}>
-                Update
-              </button>
-              <button
-                className='btn btn-outline-danger'
-                onClick={() => deleteUser(user)}>
-                Delete
-              </button>
-            </div>
-          </li>
-        ))}
-      </ul>
-    </>
-  );
+  return <div title={Title}>{Title}</div>;
 }
 
 export default App;
